8d77522a856bcc484c11aba19bc7671b520fe331
loilq <loilequang@gmail.com>
2018-12-08 15:32:58 +0700

    frist commit

--- src/main/java/com/app/rest/HomeController.java
+++ src/main/java/com/app/rest/HomeController.java
@@ -3,12 +3,14 @@ package com.app.rest;
 import com.app.response.ApiResponse;
 import com.app.utils.Api;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    @GetMapping
     public ResponseEntity<ApiResponse> getHome(){
         return Api.getSuccess("Hello Client ");
     }
