3b6a9aad7957472b28e1e12a9eef658b2b2c4525
loilq <loilequang@gmail.com>
2018-12-09 09:53:19 +0700

    env setting

--- src/main/java/com/app/rest/HomeController.java
+++ src/main/java/com/app/rest/HomeController.java
@@ -2,6 +2,7 @@ package com.app.rest;
 
 import com.app.response.ApiResponse;
 import com.app.utils.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    @Value("${evn.name}")
+    private String envName ;
     @GetMapping
     public ResponseEntity<ApiResponse> getHome(){
-        return Api.getSuccess("Hello Client ");
+        return Api.getSuccess("Hello Client. Server Api started with " + envName);
     }
 }
